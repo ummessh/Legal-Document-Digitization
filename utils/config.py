3861919d2a5f4c2c2af8828b6c71b9ff83cec164
Aryan Tandon <aryantandon2323@gmail.com>
2025-02-22 17:31:12 +0530

    Update config.py
    removed database and indicbart config

--- utils/config.py
+++ utils/config.py
@@ -5,7 +5,6 @@ class Config:
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     MODEL_DIR = os.path.join(BASE_DIR, "../models")
     OCR_DIR = os.path.join(BASE_DIR, "../ocr")
-    DB_PATH = os.path.join(BASE_DIR, "../database/ocr_results.db")
 
     # YOLO Model
     model_path = os.path.join(MODEL_DIR, "best.pt")
@@ -15,17 +14,3 @@ class Config:
     # OCR Settings
     ocr_languages = "eng+hin+mar"  # Limit OCR to these languages
     ocr_psm = 6  # Default Page Segmentation Mode
-
-    # IndicBART-based Text Correction
-    USE_INDICBART_CORRECTION = True  # Enable or disable IndicBART correction
-    INDICBART_MODEL = "ai4bharat/IndicBART"  # IndicBART model from Hugging Face
-
-    # Database
-    TABLE_CREATION_QUERY = """
-    CREATE TABLE IF NOT EXISTS ocr_results (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        bbox TEXT,
-        raw_text TEXT,
-        corrected_text TEXT NULL
-    );
-    """
